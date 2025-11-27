97a4fc9f581bbf1f42910a05c8776c82f5b79c0c
nicolas-riera <nicolas.riera@laplateforme.io>
2025-11-27 11:10:11 +0100

    Job 01

---
+++ job01.py
@@ -0,0 +1,17 @@
+def draw_rectangle(w, h):
+    for i in range(h):
+
+        ligne = "|"
+
+        if i == 0 or i == h-1:
+            for i in range(w-2):
+                ligne += "-"           
+        else:
+            for i in range(w-2):
+                ligne += " "
+
+        ligne += "|"
+
+        print(ligne)
+
+draw_rectangle(10, 3)
